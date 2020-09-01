b9b8feea6b53a1b1f2ac61ff6d482b27f299a0d7
Sara Kane <kane.sarak@gmail.com>
2020-09-01 11:59:46 -0700

    Add code from building a functional application (part 2)
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- src/power-plant.js
+++ src/power-plant.js
@@ -1,10 +1,10 @@
-class Plant {
-  constructor() {
-    this.water = 0;
-    this.soil = 0;
-    this.light = 0;
-  }
-}
+// class Plant {
+//   constructor() {
+//     this.water = 0;
+//     this.soil = 0;
+//     this.light = 0;
+//   }
+// }
 
 /* const hydrate = (plant) => {  
   return {
@@ -33,8 +33,18 @@ class Plant {
 //     [property]: (state[property] || 0) + value
 //   }
 // }
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState); //const newState = blueFood(currentState);
+    currentState = {...newState}; //{soil:5} 
+    return newState;
+  }
+}
 
-const changeState = (prop) => {
+export const stateControl = storeState();
+
+export const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
@@ -43,12 +53,30 @@ const changeState = (prop) => {
   }
 }
 
-let plant = { soil: 0, light: 0, water: 0}
+export const feed = changeState("soil")(1);
+export const blueFood = changeState("soil")(5);
+
+export const hydrate = changeState("water")(1);
+export const superWater = changeState("water")(5);
+
+// let plant = { soil: 0, light: 0, water: 0}
+// const blueFood = changeState("soil")(5)  //{soil:5}
+// const greenFood = changeState("soil")(10)
+// const yuckyFood = changeState("soil")(-5)
+
+// const fedPlant = stateControl(blueFood);
+
+// const plantFedAgain = stateControl(greenFood);
+
+// console.log(fedPlant)
+// console.log(plantFedAgain)
+
+
 // console.log(plant)
 //console.log(changeState(plant, "soil", 3))
 //console.log(plant)
 
 // const feed = changeState("soil")(5)(plant)
 // console.log(feed)
-const hydrate = changeState("soil")
-console.log(hydrate(3)(plant))
+// const hydrate = changeState("soil")
+// console.log(hydrate(3)(plant))
